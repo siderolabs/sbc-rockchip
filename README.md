50e082efedd0fd32ca7f221e8a26021e77600633
Giau. Tran Minh <hello@giautm.dev>
2024-03-30 11:16:39 +0700

    feat: support `orangepi-r1-plus-lts`
    Build a Talos overlay for the OrangePi R1 Plus LTS SBC.
    
    Signed-off-by: Noel Georgi <git@frezbo.dev>

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ This repo provides the overlay for RockChip based Talos image.
 
 ## Supported Overlay
 
-| Overlay Name | Board                 | Description                                   |
-| ------------ | --------------------- | --------------------------------------------- |
-| nanopi-r4s   | NanoPi R4S            | Overlay for NanoPi R4S                        |
-| rock64       | Pine64 Rock64         | Overlay for Pine64 Rock64                     |
-| rockpi4      | Rock Pi 4A,Rock Pi 4B | Generic overlay for Rock Pi 4A and Rock Pi 4B |
-| rockpi4c     | Rock Pi 4C            | Overlay for Rock Pi 4C                        |
-| rock4cplus   | Radxa ROCK 4C+        | Overlay for Radxa ROCK 4C+                    |
+| Overlay Name         | Board                 | Description                                   |
+| -------------------- | --------------------- | --------------------------------------------- |
+| nanopi-r4s           | NanoPi R4S            | Overlay for NanoPi R4S                        |
+| orangepi-r1-plus-lts | Orange Pi R1 Plus LTS | Overlay for Orange Pi R1 Plus LTS             |
+| rock64               | Pine64 Rock64         | Overlay for Pine64 Rock64                     |
+| rockpi4              | Rock Pi 4A,Rock Pi 4B | Generic overlay for Rock Pi 4A and Rock Pi 4B |
+| rockpi4c             | Rock Pi 4C            | Overlay for Rock Pi 4C                        |
+| rock4cplus           | Radxa ROCK 4C+        | Overlay for Radxa ROCK 4C+                    |
---
+++ installers/orangepi-r1-plus-lts/src/main.go
@@ -0,0 +1,83 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/siderolabs/go-copy/copy"
+	"github.com/siderolabs/talos/pkg/machinery/overlay"
+	"github.com/siderolabs/talos/pkg/machinery/overlay/adapter"
+	"golang.org/x/sys/unix"
+)
+
+const (
+	off int64 = 512 * 64
+	dtb       = "rockchip/rk3328-orangepi-r1-plus-lts.dtb"
+)
+
+func main() {
+	adapter.Execute(&opiR1PlusLTS{})
+}
+
+type opiR1PlusLTS struct{}
+
+type opiR1PlusLTSExtraOptions struct{}
+
+func (i *opiR1PlusLTS) GetOptions(extra opiR1PlusLTSExtraOptions) (overlay.Options, error) {
+	return overlay.Options{
+		Name: "orangepi-r1-plus-lts",
+		KernelArgs: []string{
+			"console=tty0",
+			"console=ttyS2,1500000n8",
+			"sysctl.kernel.kexec_load_disabled=1",
+			"talos.dashboard.disabled=1",
+		},
+		PartitionOptions: overlay.PartitionOptions{
+			Offset: 2048 * 10,
+		},
+	}, nil
+}
+
+func (i *opiR1PlusLTS) Install(options overlay.InstallOptions[opiR1PlusLTSExtraOptions]) error {
+	var f *os.File
+
+	f, err := os.OpenFile(options.InstallDisk, os.O_RDWR|unix.O_CLOEXEC, 0o666)
+	if err != nil {
+		return fmt.Errorf("failed to open %s: %w", options.InstallDisk, err)
+	}
+
+	defer f.Close() //nolint:errcheck
+
+	uboot, err := os.ReadFile(filepath.Join(options.ArtifactsPath, "arm64/u-boot/orangepi-r1-plus-lts/u-boot-rockchip.bin"))
+	if err != nil {
+		return err
+	}
+
+	if _, err = f.WriteAt(uboot, off); err != nil {
+		return err
+	}
+
+	// NB: In the case that the block device is a loopback device, we sync here
+	// to esure that the file is written before the loopback device is
+	// unmounted.
+	err = f.Sync()
+	if err != nil {
+		return err
+	}
+
+	src := filepath.Join(options.ArtifactsPath, "arm64/dtb", dtb)
+	dst := filepath.Join(options.MountPrefix, "/boot/EFI/dtb", dtb)
+
+	err = os.MkdirAll(filepath.Dir(dst), 0o600)
+	if err != nil {
+		return err
+	}
+
+	return copy.File(src, dst)
+}
