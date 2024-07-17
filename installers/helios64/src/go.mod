1eef47f8fe45cf0cbf211f6fa477cc1196b04577
Hemanth Bollamreddi <blmhemu@gmail.com>
2024-07-17 09:08:02 +0530

    feat: add support for helios64
    Add support for kobol helios64 board. It does not have a mainline u-boot
    and hence needs to be patched in.
    
    Signed-off-by: Hemanth Bollamreddi <blmhemu@gmail.com>

---
+++ installers/helios64/src/go.mod
@@ -0,0 +1,11 @@
+module helios64
+
+go 1.22.4
+
+require (
+	github.com/siderolabs/go-copy v0.1.0
+	github.com/siderolabs/talos/pkg/machinery v1.7.5
+	golang.org/x/sys v0.21.0
+)
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ installers/helios64/src/main.go
@@ -0,0 +1,84 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/siderolabs/go-copy/copy"
+	"github.com/siderolabs/talos/pkg/machinery/overlay"
+	"github.com/siderolabs/talos/pkg/machinery/overlay/adapter"
+	"golang.org/x/sys/unix"
+)
+
+const (
+	off int64 = 512 * 64
+	dtb       = "rockchip/rk3399-kobol-helios64.dtb"
+)
+
+func main() {
+	adapter.Execute(&helios64{})
+}
+
+type helios64 struct{}
+
+type helios64ExtraOptions struct{}
+
+func (i *helios64) GetOptions(extra helios64ExtraOptions) (overlay.Options, error) {
+	return overlay.Options{
+		Name: "helios64",
+		KernelArgs: []string{
+			"console=tty1",
+			"console=ttyS2,1500000n8",
+			"usb-storage.quirks=0x2537:0x1066:u,0x2537:0x1068:u",
+			"sysctl.kernel.kexec_load_disabled=1",
+			"talos.dashboard.disabled=1",
+		},
+		PartitionOptions: overlay.PartitionOptions{
+			Offset: 2048 * 10,
+		},
+	}, nil
+}
+
+func (i *helios64) Install(options overlay.InstallOptions[helios64ExtraOptions]) error {
+	var f *os.File
+
+	f, err := os.OpenFile(options.InstallDisk, os.O_RDWR|unix.O_CLOEXEC, 0o666)
+	if err != nil {
+		return fmt.Errorf("failed to open %s: %w", options.InstallDisk, err)
+	}
+
+	defer f.Close() //nolint:errcheck
+
+	uboot, err := os.ReadFile(filepath.Join(options.ArtifactsPath, "arm64/u-boot/helios64/u-boot-rockchip.bin"))
+	if err != nil {
+		return err
+	}
+
+	if _, err = f.WriteAt(uboot, off); err != nil {
+		return err
+	}
+
+	// NB: In the case that the block device is a loopback device, we sync here
+	// to esure that the file is written before the loopback device is
+	// unmounted.
+	err = f.Sync()
+	if err != nil {
+		return err
+	}
+
+	src := filepath.Join(options.ArtifactsPath, "arm64/dtb", dtb)
+	dst := filepath.Join(options.MountPrefix, "/boot/EFI/dtb", dtb)
+
+	err = os.MkdirAll(filepath.Dir(dst), 0o600)
+	if err != nil {
+		return err
+	}
+
+	return copy.File(src, dst)
+}
